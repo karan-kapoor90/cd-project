040bd0bf7071ba63e577ee7d5e0d8f195b99f13c
Concourse <karan.kapoor.work@gmail.com>
2020-05-22 03:44:36 +0530

    Updated pipelining config

--- scripts/create-commit.sh
+++ scripts/create-commit.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cd cd-resources
 
-git status
 
-cd ../
+cp -r cd-resources/ compiled-resources/
 
-cp -r target-resources/ compiled-resources/
+#git status
+
+#cd ../
+
+cp -r target-resources/ compiled-resources/kube/
 
 cd compiled-resources
-
+git status
 git config --global user.email "karan.kapoor.work@gmail.com"
 git config --global user.name "Concourse"
 
