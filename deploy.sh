159c3c14908cf478b123baacee60ceef5258f154
Concourse <karan.kapoor.work@gmail.com>
2020-05-22 03:19:11 +0530

    Updated pipelining config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.email "karan.kapoor.work@gmail.com"
+git config --global user.name "Concourse"
+
 git add .
 git commit -m "Updated pipelining config"
 git push origin master
